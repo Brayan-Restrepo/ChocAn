f4f8cc8c51f8e67ea8898d811654026fa2f819f0
Brayan-Restrepo <brayan.restrepo@unillanos.edu.co>
2017-05-13 18:30:06 -0500

    -

---
+++ ChocAn/src/Entidad/ChocAn.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Entidad;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Brayan
+ */
+public class ChocAn {
+
+    public static List<Miembro> listaMiembro = new ArrayList<Miembro>();
+    public static List<Proveedor> listaProveedor = new ArrayList<Proveedor>();
+    public static List<Servicio> listaServicio = new ArrayList<Servicio>();
+    public static List<Factura> listaFactura = new ArrayList<Factura>();
+    public static List<Factura> listaReporteMiembro = new ArrayList<Factura>();
+
+
+    //listaarray.add(new Persona(5,"Ruben",121)); 	Inaertar
+    //listaarray.remove(listaarray.get(i)); 		Eliminar    
+    //listaarray.size() 				Tamaño
+    //listaarray.get(i).altura 				Buscar
+    
+    public ChocAn() {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
